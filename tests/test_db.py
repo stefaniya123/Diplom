3d576bd6581449da532333cf2737f20aa7545083
Stefaniya <foxzz@bk.ru>
2025-12-09 00:52:36 +0300

    убран CI

--- tests/test_db.py
+++ tests/test_db.py
@@ -3,35 +3,12 @@ import allure
 import mysql.connector
 from mysql.connector import Error
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
-import os
 
-def get_chrome_options():
-    """Настройка ChromeOptions для работы в CI"""
-    options = Options()
-    options.add_argument("--start-maximized")
-    options.add_argument("--no-sandbox")           # Обязательно для CI
-    options.add_argument("--disable-dev-shm-usage") # Обязательно для CI
-    options.add_argument("--headless=new")         # Безголовый режим
-    options.add_argument("--disable-gpu")          # Для стабильности
-    options.add_argument("--disable-software-rasterizer")
-    options.add_argument("--disable-web-security")
-    options.add_argument("--disable-extensions")
-    options.add_argument("--disable-popup-blocking")
-    options.add_argument("--disable-notifications")
-    options.add_argument("--disable-background-timer-throttling")
-    options.add_argument("--disable-background-timer-throttling")
-    options.add_argument("--disable-background-timer-throttling")
-    options.add_argument("--disable-background-timer-throttling")
-    options.add_argument("--disable-background-timer-throttling")
-    options.add_argument("--disable-background-timer-throttling")
-    options.add_argument("--disable-background-timer-throttling")
-    options.add_argument("--disable-background-timer-throttling")
-    options.add_argument("--disable-background-timer-throttling")
-    options.add_argument("--disable-background-timer-throttling")
-    options.add_argument("--disable-background-timer-throttling")
 
 def get_db_connection():
     try:
